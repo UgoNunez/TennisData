b18280e22e9b2277d828290d3197d5cc5d42fd9e
UgoNunez <31165390+UgoNunez@users.noreply.github.com>
2017-08-30 22:00:01 -0300

    In this commit I added the lines 7 and 8, asking the user to enter the data.

--- tennisdata/src/tennisdatapack/adm.java
+++ tennisdata/src/tennisdatapack/adm.java
@@ -4,5 +4,7 @@ public class adm {
 	public static void main(String[] args){
 		System.out.println("Please, enter the tennis data");
 		System.out.println("Heeyyyy");
+		//This is my last contribution to the project:
+		System.out.println("Please enter your data");
 	}
 }
