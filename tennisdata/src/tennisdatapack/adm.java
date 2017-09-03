045144f5c56ea112153154d29e7d3d68ec335e02
UgoNunez <31165390+UgoNunez@users.noreply.github.com>
2017-09-02 22:09:01 -0300

    Added the scanner to read the user and password from the user.

--- tennisdata/src/tennisdatapack/adm.java
+++ tennisdata/src/tennisdatapack/adm.java
@@ -1,13 +1,21 @@
 package tennisdatapack;
 
+import java.util.Scanner;
+
 public class adm {
 	public static void main(String[] args){
-		System.out.println("Please, enter the tennis data");
-		System.out.println("Heeyyyy");
-		//This is my last contribution to the project:
-		System.out.println("Please enter your data");
+		System.out.println("Welcome to Tennis Data");
+		System.out.println("");
 		//It's important that the user log in.
-		System.out.println("Enter your user name: ");
-		System.out.println("Enter your password: ");
+		System.out.print("Enter your user name: ");
+		Scanner scanner = new Scanner(System.in);
+		String username = scanner.nextLine();
+		System.out.print("Enter your password: ");
+		String password = scanner.nextLine();
+		System.out.println("");
+		System.out.println("Los datos ingresados fueron: ");
+		System.out.println("Username = " + username);
+		System.out.println("Password = " + password);
+		scanner.close();
 	}
 }
